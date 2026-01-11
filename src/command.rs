b383ae9e147f86d4fdd40c72b00899ef3037713a
wuliya <3369906077@qq.com>
2026-01-11 22:22:17 +0800

    chore: 更新参数使用方法

--- src/command.rs
+++ src/command.rs
@@ -1,13 +1,9 @@
 use puniyu_plugin::prelude::*;
 
-#[command(
-    name = "echo",
-    desc = "输出一段文本",
-    args = ["msg"]
-)]
-async fn echo(bot: &BotContext, ev: &MessageContext) -> HandlerResult {
-	if let Some(msg) = ev.arg("msg") && !msg.is_empty() {
-		bot.reply(msg.into()).await?;
-	}
-	Ok(().into())
+#[command(name = "echo", desc = "输出一段文本")]
+#[arg(name = "msg", desc = "要输出的文本")]
+async fn echo(ctx: &MessageContext) -> HandlerResult<HandlerAction> {
+    let msg = ctx.arg("msg").and_then(|m| m.as_str()).unwrap();
+    ctx.reply(msg.into()).await?;
+    Ok(().into())
 }
